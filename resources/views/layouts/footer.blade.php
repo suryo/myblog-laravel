86d222388adfd5dac2700f3f0f366d1ea8ee1eb1
suryo <suryoatm@gmail.com>
2023-03-04 17:14:38 +0700

    clean code

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Supresso.
+                <script>document.write(new Date().getFullYear())</script> © Vasusena.
             </div>
             {{-- <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
